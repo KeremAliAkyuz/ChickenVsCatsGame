11c17019b750934ecd8d7485bd9c252fc0dea379
KeremAliAkyuz <109032609+KeremAliAkyuz@users.noreply.github.com>
2023-01-15 23:23:02 +0300

    Add files via upload

---
+++ Chiken/src/GameFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+ 
+    GameFrame(){
+	
+	GamePanel panel = new GamePanel();
+	
+	this.add(panel);
+	this.setTitle("Chiken vs Cats");
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setResizable(false);
+	this.pack();
+	this.setVisible(true);
+	this.setLocationRelativeTo(null);
+	
+	
+	
+	
+}
+
+
+
+
+}
